6554cb1fd38dfdb2f7afe2cb22a0e71a3e8240c1
kthana-bg <kthana7503@gmail.com>
2026-05-14 08:01:51 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# Install system dependencies
-apt-get update
-apt-get install -y libgl1-mesa-glx libglib2.0-0 libsm6 libxext6 libxrender-dev libgomp1
 
 # Upgrade pip
 pip install --upgrade pip
 
-# Install tensorflow separately (in case of issues)
-pip install tensorflow==2.15.0
+pip install streamlit-webrtc==0.45.1
+
+# Install other dependencies
+pip install -r requirements.txt
